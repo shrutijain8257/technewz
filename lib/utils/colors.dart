81e41be269ef634c1cf6bf22fb9202b535cf9136
shrutijain8257 <shrutijain9540@gmail.com>
2023-02-19 01:54:27 +0530

    some modifications

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -4,7 +4,7 @@ class AppColors {
   static Color primary = Colors.deepPurple.shade800;
   static Color lightwhite = Colors.grey.shade100;
   static Color bgColor = const Color(0xff0d0d0d);
-  static Color black = Colors.black;
+  static Color black = Colors.black87;
   static Color white = Colors.white;
   static Color darkgrey = Colors.grey.shade900;
 }
